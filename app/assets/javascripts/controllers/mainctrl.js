516d10b60081639c968ef8edaadfb1f4ff43aeba
Roger Kelly <loraxman@gmail.com>
2014-12-22 15:56:47 -0600

    fixed dynamic divs for rapheal/angular

--- app/assets/javascripts/controllers/mainctrl.js
+++ app/assets/javascripts/controllers/mainctrl.js
@@ -37,6 +37,9 @@ dataeng.controller('FirstCtrl', ['$scope', function($scope) {
 //below directive allows raphel to have a div passed in 
 //so that it can attach to the div at runtime from angular
 dataeng.directive('piechart', function() {
+	// assign a sequence to make the divid so that rapahel can 
+	// find one per piechart
+	 var uniqueId = 1;
     return {
         restrict: 'E',
         scope: {
@@ -45,71 +48,85 @@ dataeng.directive('piechart', function() {
             
         },
         template: function(element,attrs) {
-        	return '<div id="'+attrs.divname+'"' +' style="width:200"></div>';
+        	// make the div id a sequence based on the var above
+        	return '<div id="'+"{{uniqueId}}"+'"' +' style="width:200"></div>';
         },
         	
         link: function(scope, element, attrs) {
         	//console.log(scope.waits);
-            var r = Raphael(attrs.divname, 450,200);
-            var back = r.rect(0,0,450,200,5);
-            back.attr({'fill':'#C7C7C7'});
-            var x=25;
-            var colors=['#F2676C','green','#5BAAE6', 'yellow','orange','grey'];
-            
-            var circles={};
-            var textpop="";
-            for (var i in scope.waits) {
-            	
-            	var y = Math.random()*170;
-            	circles[scope.waits[i].name] = r.circle(x,y,20); 
-            	circles[scope.waits[i].name].attr({'fill':colors[i]});
+        	scope.uniqueId = 'item' + uniqueId++;	// set the next divname
 
-            	circles[scope.waits[i].name].data({'custom':"run after this: \n"});
-            	circles[scope.waits[i].name].mouseover(function nodeover(i) {
-            	
-            		this.animate({'r':30},250);
-            		var textstr = "waiting on stuff";
-            		textpop = this.paper.text(400,50,this.data('custom'));
-           		
-            	});
-            	circles[scope.waits[i].name].mouseout(function nodeover() {
-            		
-            		this.animate({'r':20},250);
-            		textpop.remove();
-            		
-            	});
-            	r.text(x+5, y, scope.waits[i].name);
-            	x+=70;
-            }
-            //connect
-            for (var i in scope.waits) {
-            	// get a node
-            	// do  line as to who waits on me to other nodes
-            	var node1 = scope.waits[i];
-            	for (var j in node1.waiting_on) {
-            		//console.log(circles[node1.waiting_on[j]]);
-            		var cl = circles[node1.waiting_on[j]];
-            		var pt1x = cl.attr('cx');
-            		var pt1y = cl.attr('cy');
-            		var clthis = circles[node1.name];
-            		var pt2x = clthis.attr('cx');
-            		var pt2y = clthis.attr('cy');
-            		var line  = r.path( ["M", pt1x, pt1y, "L", pt2x,pt2y ]);
-            		line.attr({'stroke':colors[i], 'opacity':.45});
-            		var midx = (pt1x+pt2x)/2;
-            		var midy = (pt1y+pt2y)/2;
-            		var linelabel = r.text(midx,midy,node1.name);
-            		linelabel.attr({'stroke':colors[i]});
-            		var datstr = circles[node1.name].data('custom');
-            		datstr = datstr + " " + node1.waiting_on[j];
-                	circles[node1.name].data({'custom':datstr});
-
-            		//cl.attr({'fill':colors[i]});
-            	}
-            	
-            }
-            	
-        }
+        	//VERY IMPORTANT!! let Angular watch for changes on this div so that
+        	//when it is created we do our Raphael animation
+        	scope.$watch(scope.uniqueId, function (val) {
+	            var r  = new Raphael(scope.uniqueId, 450,200);
+	            var back = r.rect(0,0,450,200,5);
+	            back.attr({'fill':'#C7C7C7'});
+	            var x=25;
+	            var colors=['#F2676C','green','#5BAAE6', 'yellow','orange','grey'];
+	            
+	            var circles={};
+	            var textpop="";
+	            for (var i in scope.waits) {
+	            	
+	            	var y = Math.random()*170;
+	            	console.log(x);
+	            	console.log(y);
+	            	circles[scope.waits[i].name] = r.circle(x,y,20); 
+	            	circles[scope.waits[i].name].attr({'fill':colors[i]});
+	
+	            	circles[scope.waits[i].name].data({'custom':"run after this: \n"});
+	            	circles[scope.waits[i].name].mouseover(function nodeover(i) {
+	            	
+	            		this.animate({'r':30},250);
+	            		var textstr = "waiting on stuff";
+	            		textpop = this.paper.text(400,50,this.data('custom'));
+	           		
+	            	});
+	            	circles[scope.waits[i].name].mouseout(function nodeover() {
+	            		
+	            		this.animate({'r':20},250);
+	            		textpop.remove();
+	            		
+	            	});
+	            	console.log(x);
+	            	console.log(y);
+	            	
+	            	var t1 = r.text(x-8, y/2, scope.waits[i].name);
+	            	t1.attr({'text-anchor':'start'});
+	            
+	            	x+=70;
+	            }
+	            //connect
+	            for (var i in scope.waits) {
+	            	// get a node
+	            	// do  line as to who waits on me to other nodes
+	            	var node1 = scope.waits[i];
+	            	for (var j in node1.waiting_on) {
+	            		//console.log(circles[node1.waiting_on[j]]);
+	            		var cl = circles[node1.waiting_on[j]];
+	            		var pt1x = cl.attr('cx');
+	            		var pt1y = cl.attr('cy');
+	            		var clthis = circles[node1.name];
+	            		var pt2x = clthis.attr('cx');
+	            		var pt2y = clthis.attr('cy');
+	            		var line  = r.path( ["M", pt1x, pt1y, "L", pt2x,pt2y ]);
+	            		line.attr({'stroke':colors[i], 'opacity':.45});
+	            		var midx = (pt1x+pt2x)/2;
+	            		var midy = (pt1y+pt2y)/2;
+	            		var linelabel = r.text(midx,midy/2,node1.name);
+	            		linelabel.attr({'stroke':colors[i]});
+	            		var datstr = circles[node1.name].data('custom');
+	            		datstr = datstr + " " + node1.waiting_on[j];
+	                	circles[node1.name].data({'custom':datstr});
+	
+	            		//cl.attr({'fill':colors[i]});
+	            	}
+	            	
+	            }
+	            	
+	        }
+	   )}
     }
 });
 
